354a2f59808b4ef82f178311cfbe1e0322374ce1
e-s-l <e-sullivan-lester@proton.me>
2024-06-14 11:19:12 +0200

    additional preprocessing, just rough for now

---
+++ strip-output.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# strip file
+# assuming ran get-exp-echedules.sh > output.txt
+# then run this as strip-output.sh output.txt
+# to produce a plain list "exps.txt" of exp_code doy start_time duration
+# (duration to be added...)
+
+FILE=$1
+
+NEW_FILE="exps.txt"
+
+grep -E '[A-Za-z]{1,3}[0-9]{2,5} [0-9]{1,3} [0-9]{2}:[0-9]{2}' "$FILE" >> $NEW_FILE
